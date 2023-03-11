cfb134d0ab7138dc795327979eb411d650f9b4e9
xmrdldkdlel <xmrdldkdlel@gmail.com>
2023-03-11 22:23:35 +0900

    No.2

--- utils.py
+++ utils.py
@@ -7,12 +7,5 @@
 """
 
 
-def factorial(x):
-    if x <= 1:
-        return 1
-    return x * factorial(x-1)
-
-
-def gugudan(x):
-    for i in range(9):
-        print((i+1)*x)
+def jinny(x) :
+    return x+1
